e4d3ee8c196757bb630decc4d741509e2a31a467
Tritiac Ion <itritiac@MacBook-Pro-Tritiac.local>
2020-06-17 10:34:06 +0300

    add react-redux bibl

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import * as serviceWorker from './serviceWorker';
-import store from "./redux/redux-store";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import './index.css';
+import store from "./redux/redux-store";
+import * as serviceWorker from './serviceWorker';
 
 let rerenderEntireTree = (state) => {
-    debugger
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
+            <Provider store = {store}>
+                <App/>
+            </Provider>
         </BrowserRouter>, document.getElementById('root'));
 }
 
-rerenderEntireTree(store.getState());
+rerenderEntireTree();
 
-store.subscribe(() => {
+store.subscribe(() =>{
     let state = store.getState();
     rerenderEntireTree(state);
+
 });
 
 
