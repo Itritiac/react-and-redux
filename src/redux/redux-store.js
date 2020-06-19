6822586e05e760fb626bb6df881dc19e5938dd5c
Tritiac Ion <itritiac@MacBook-Pro-Tritiac.local>
2020-06-19 14:42:27 +0300

    add auth

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,18 +2,18 @@ import {combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
-import userReducer from "./user-reducer";
+import usersReducer from "./user-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
-    usersPage: userReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
+
 let store = createStore(reducers);
-
 window.store = store;
-
-
 export default store;
