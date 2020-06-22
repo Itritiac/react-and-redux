04cb608beff8f8c20149f9d07f2f299cb1a09f9c
Itritiac <itritiac@gmail.com>
2020-06-22 15:40:29 +0300

    refactoring

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+  import React from 'react';
 import {connect} from 'react-redux';
 import {
     follow,
@@ -9,34 +9,30 @@ import {
     unfollow
 } 
 from './../../redux/user-reducer';
-import * as axios from 'axios';
 import Users from './Users';
 import Preloader from "../common/Preloader/Preloader";
+import {userAPI } from '../api/api';
 
 class UsersContainer extends React.Component {
   componentDidMount() {
       this.props.toggleIsFetching(true);
-      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,{
-          withCredentials: true,
-          
-          
-      })
-          .then(response => {
-              this.props.toggleIsFetching(false);
-              this.props.setUsers(response.data.items);
-              this.props.setTotalUsersCount(40);
-          });
+
+      userAPI.getUsers(this.props.currentPage, this.props.pageSize)
+        .then(data => {
+            debugger
+            this.props.toggleIsFetching(false);
+            this.props.setUsers(data.items);
+            this.props.setTotalUsersCount(40);
+        });
   }
 
   onPageChanged = (pageNumber) => {
       this.props.setCurrentPage(pageNumber);
       this.props.toggleIsFetching(true);
-      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,{
-          withCredentials: true
-      })
-          .then(response => {
+      userAPI.getUsers(pageNumber, this.props.pageSize)
+          .then(data => {
               this.props.toggleIsFetching(false);
-              this.props.setUsers(response.data.items);
+              this.props.setUsers(data.items);
           });
   }
 
---
+++ src/components/api/api.js
@@ -0,0 +1,24 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL : 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    "API-KEY" : "77ab2db8-601f-459a-b48d-78248b31ac9d"
+  }
+})
+
+export const userAPI = {
+  getUsers(currentPage = 1, pageSize = 6){
+
+    return(
+      instance.get(`users?page=${currentPage}&count=${pageSize}`,{
+            withCredentials: true,
+        })
+        .then(response => {
+          return response.data;
+        })
+    )
+}
+}
+
