c658299788dc95cdf6757b793b1b1c92a6635e4c
Tritiac Ion <itritiac@MacBook-Pro-Tritiac.local>
2020-06-25 15:28:20 +0300

    refactoring

--- src/redux/user-selectors.js
+++ src/redux/user-selectors.js
@@ -19,6 +19,7 @@ export const getIsFetching = (state) => {
 }
 
 export const getFollowingInProgress = (state) => {
-    return state.usersPage.followingInProgress;
+    return state.usersPage.isFetching;
 }
+
 
